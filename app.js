c618d0da435cfc16a6a53ec19ecbadf0e766eae0
Squall <squall.f@gmail.com>
2020-12-27 00:05:09 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var exphbs = require('express-handlebars');
+var http = require('http');
+var app = express();
+var DB = require("./lib/db.js");
+
+var todoRouter = require('./routes/todo');
+
+var init_web = function () {
+    http.createServer(app).listen(port, function () {
+        console.log("Express server listening on port " + port);
+    });
+};
+
+var init_db_emitter = DB.init();
+
+init_db_emitter.on("ok", function () {
+    init_web();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function (req, res, next) {
+    res.redirect('/todo');
+});
+  
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    helpers: require('./config/handlebars-helpers')
+
+}));
+
+app.set('view engine', '.hbs');
+
+app.use('/todo', todoRouter);
+
+exports = module.exports = app;
---
+++ routes/todo.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+var DB = require('../lib/db.js');
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  let emitter = DB.getTodo();
+  
+  emitter.on("ok", function (data) {
+    let d = JSON.parse(JSON.stringify(data));
+    res.render('home',{todos: d});
+  });
+});
+
+router.get('/edit/:id', function(req, res) {
+  let id = req.params.id;
+
+  let emitter = DB.getTodo();
+  
+  emitter.on("ok", function (data) {
+    let d = JSON.parse(JSON.stringify(data));
+    res.render('edit',{editId: id, todos: d});
+  });
+});
+
+router.post('/create', function(req, res) {
+  let data = {content:req.body.content};
+  let emitter = DB.createTodo(data);
+  
+  emitter.on("ok", function (data) {
+    //res.json({ status: 200, data: data });
+    res.redirect( '/todo' );
+  });
+
+  emitter.on("err", function(err){
+    console.log(err);
+    res.json({stauts: 403, data: err});
+  })
+  
+});
+
+
+router.post('/update/:id', function(req, res) {
+  let data = {content:req.body.content};
+  let cond = {_id:req.params.id};
+  let emitter = DB.updateTodo(data, cond);
+  
+  emitter.on("ok", function (data) {
+    //res.json({ status: 200, data: data });
+    res.redirect( '/todo' );
+  });
+
+  emitter.on("err", function(err){
+    console.log(err);
+    res.json({stauts: 403, data: err});
+  })
+  
+});
+
+router.get('/delete/:id', function(req, res) {
+  let cond = {_id:req.params.id};
+  let emitter = DB.deleteTodo(cond);
+  
+  emitter.on("ok", function (data) {
+    res.redirect( '/todo' );
+  });
+
+  emitter.on("err", function(err){
+    console.log(err);
+    res.json({stauts: 403, data: err});
+  })
+  
+});
+
+
+module.exports = router;
